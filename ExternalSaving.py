0ccbb8f0ed439bf912c0ecdeb562d92b5ef960d2
Sherif-Ahmed17 <66403092+Sherif-Ahmed17@users.noreply.github.com>
2021-09-22 20:31:11 +0200

    Add files via upload

---
+++ ExternalSaving.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# df = pd.read_json('E:\\finalfbscrapping\\Testing.json')
+# df.to_excel('E:\\TestingFBgroupScrappedData.xlsx')
+
+df = pd.read_json('E:\\finalfbscrapping\\Myjson.json')
+df.to_excel('E:\\MyjsonFBgroupScrappedData.xlsx')
+   
+  
